ddf7651972be02cc3f79d9ebd3a8e7f31aaa76e0
vadimpilyugin <vadimpilyugin@gmail.com>
2016-11-04 16:45:47 +0300

    finish time

--- src/solve.cpp
+++ src/solve.cpp
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include "mpi.h"
 
-#define debug 0
+#define debug 1
 
 int rank, proc_num;
 
@@ -58,54 +58,17 @@ void sieve_non_rec(char *res, int n)
 					res[to_i(j, 1)] = 0;
 		}
 }
-// void sieve_first_n(int n, char *res)
-//  {
-//  	int k, offset;
-//  	if(n <= 100)
-//  		sieve_non_rec(res, n);
-//  	else
-//  	{
-//  		int sqr_n = int(floor(sqrt(n))); //количество чисел до корня из n
-//  		sieve(sqr_n, res); //получаем первые sqr_n простых чисел
-//  		int part_size = int(floor((float(n) - sqr_n)/proc_num)); //делим оставшуюся область, каждому поровну
-//  		int ar[2]; ar[0] = sqr_n; ar[1] = part_size; //передаем начальное количество простых чисел и размер части
-//  		MPI_Bcast(ar, 2, MPI_INT, 0, MPI_COMM_WORLD); //всем рабам и мастеру выделить буфер для приема простых чисел
-//  		MPI_Bcast(res, sqr_n, MPI_CHAR, 0, MPI_COMM_WORLD); //собственно раздача простых чисел
- 
-//  		// каждый процесс вычисляет свою область
-//  		{
-//  			// res = malloc(ar[1])
-//  			int start_n = ar[0] + rank*ar[1] + 1;
-//  			int end_n = start_n + ar[1] - 1;
-//  			char *primes = (char*) malloc(end_n - start_n + 1);
-//  			find_pr(res, primes, start_n, end_n);
-//  			MPI_Gather(primes, ar[1], MPI_CHAR, res + ar[0], ar[1], MPI_CHAR, 0, MPI_COMM_WORLD);
-//  			free(primes);
-//  			// free(res);
-//  		}
-//  		if((offset = (n-sqr_n)%proc_num) != 0)
-//  		{
-//  			if(debug) fprintf(stderr, "Не делится %d на %d: остаток %d\n", n, proc_num, offset);
-//  			int start_n = n-offset+1;
-//  			int end_n = n;
-//  			char *primes = (char*) malloc(end_n - start_n + 1);
-//  			find_pr(res, primes, start_n, end_n);
-//  			for(k = start_n; k <= end_n; k++)
-//  				res[to_i(k, 1)] = primes[to_i(k, start_n)];
-//  			free(primes);
-//  		}
-//  	}
-//  }
-void sieve(char *res, char *primes, int start, int end)
+void sieve(char *res, char *primes, int start, int end, double *sum_time)
 {
 	int offset;
+	double start_time, finish_time;
 	int sqr_n = int(floor(sqrt(end))); // количество чисел до корня из n
 	if(sqr_n <= 100)
 		sieve_non_rec(res, sqr_n);
 	else
 	{
 		char *res_tmp = (char*) malloc(int(floor(sqrt(sqr_n)))*sizeof(char));
-		sieve(res_tmp, res, 1, sqr_n); // получаем первые sqr_n простых чисел
+		sieve(res_tmp, res, 1, sqr_n, sum_time); // получаем первые sqr_n простых чисел
 		free(res_tmp);
 	}
 		
@@ -118,10 +81,13 @@ void sieve(char *res, char *primes, int start, int end)
 	// каждый процесс вычисляет свою область
 	{
 		if(debug) fprintf(stderr, "Процесс %d получил приказ: part_size = %d, начало в %d\n", rank, ar[1], ar[2]);
+		start_time = MPI_Wtime();
 		int start_n = ar[2] + rank*ar[1];
 		int end_n = start_n + ar[1] - 1;
 		char *primes_tmp = (char*) malloc(ar[1]);
 		find_pr(res, primes_tmp, start_n, end_n);
+		finish_time = MPI_Wtime();
+		*sum_time += finish_time - start_time;
 		MPI_Gather(primes_tmp, ar[1], MPI_CHAR, primes, ar[1], MPI_CHAR, 0, MPI_COMM_WORLD);
 		free(primes_tmp);
 	}
@@ -143,34 +109,26 @@ int main (int argc, char* argv[])
 
     if(rank == 0)
     {
-    	// int n = int(1e8);
-    	// char *res = (char*) malloc(n*sizeof(char));
-    	
-  //   	sieve(n, res);
-		// int k;
-  //   	for(k = 1; k <= n; k++)
-		// 	if(res[to_i(k, 1)])
-		// 	{
-		// 		printf("%d", k);
-		// 		if(debug)
-		// 			if(!prime(k))
-		// 			{
-		// 				printf("!!!");
-		// 				if(debug) fprintf(stderr, "ERROR! k = %d\n", k);
-		// 			}
-					
-		// 		printf("\n");
-		// 	}
-
-    	int start = 100, end = 10000;
-    	int k;
+    	if(argc != 3)
+    		exit(2);
+    	int start = atoi(argv[1]), end = atoi(argv[2]);
+    	if(debug) fprintf(stderr, "Find primes at [%d, %d]\n", start, end);
+    	int k, count = 0;
     	int sqr_n = int(floor(sqrt(end)));
     	char *res = (char*) malloc(sqr_n*sizeof(char));
     	char *primes = (char*) malloc((end-start+1)*sizeof(char));
-    	sieve(res, primes, start, end);
+    	double sum_time = 0;
+    	sieve(res, primes, start, end, &sum_time);
+    	double max_time, all_time;
+		MPI_Reduce(&sum_time, &max_time, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
+		MPI_Reduce(&sum_time, &all_time, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+		fprintf(stderr, "Максимальное время среди процессов: %lf\n", max_time);
+		fprintf(stderr, "Суммарное время среди процессов: %lf\n", all_time);
+
     	for(k = start; k <= end; k++)
     		if(primes[to_i(k, start)])
     		{
+    			count++;
     			printf("%d", k);
     			if(debug)
 	    			if(!prime(k))
@@ -178,13 +136,15 @@ int main (int argc, char* argv[])
 						printf("!!!");
 						if(debug) fprintf(stderr, "ERROR! k = %d\n", k);
 					}
-		 		printf("\n");
+		 		printf(" ");
     		}
+    	fprintf(stderr, "Найдено простых: %d\n", count);
+
 
     	int ar[3]; ar[1] = 0;
     	MPI_Bcast(ar, 3, MPI_INT, 0, MPI_COMM_WORLD);
     	
-				
+
 		free(res);
 		MPI_Finalize();
 		return 0;
@@ -194,22 +154,28 @@ int main (int argc, char* argv[])
     	while(1)
     	{
     		int ar[3];
+    		double sum_time = 0, start_time, finish_time;
     		MPI_Bcast(ar, 3, MPI_INT, 0, MPI_COMM_WORLD);
     		if(ar[1] != 0)
     		{
 				char *res = (char*) malloc(ar[0] * sizeof(char));
 				MPI_Bcast(res, ar[0], MPI_CHAR, 0, MPI_COMM_WORLD);
 				if(debug) fprintf(stderr, "Процесс %d получил приказ: part_size = %d, начало в %d\n", rank, ar[1], ar[2]);
+				start_time = MPI_Wtime();
 				int start_n = ar[2] + rank*ar[1];
 				int end_n = start_n + ar[1] - 1;
 				char *primes = (char*) malloc(ar[1]);
 				find_pr(res, primes, start_n, end_n);
+				finish_time = MPI_Wtime();
+				sum_time += finish_time - start_time;
 				MPI_Gather(primes, ar[1], MPI_CHAR, primes, ar[1], MPI_CHAR, 0, MPI_COMM_WORLD);
 				free(primes);
 				free(res);
 			}
 			else
 			{
+				MPI_Reduce(&sum_time, &sum_time, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
+				MPI_Reduce(&sum_time, &sum_time, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
 				MPI_Finalize();
 				return 0;
 			}
--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 set -o errexit # остановка после первой ошибки
 export PATH=/home/vadim/mpich-install/bin:$PATH
 make all
-mpiexec -n 30 build/bin/solve 1>data/result.txt
+mpiexec -n 15 build/bin/solve 100 10000 1>data/result.txt
